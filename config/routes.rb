a478ff6f30684387cdc64055be0c15da7993951e
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-04-24 00:22:35 -0500

    call back page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'user/login'
 
   get 'auth/facebook', as: "auth_provider"
-  get 'auth/facebook/callback', to: 'users#login'
+  get 'welcome/index', to: 'users#login'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

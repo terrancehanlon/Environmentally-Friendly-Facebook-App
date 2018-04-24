692341fef6ad66e4e4debe9126c56ae2520a85b1
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-04-24 00:12:03 -0500

    call back page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook, 584340308588152, 451de733ddb3998eee829a17ebaf6ca1, {:scope =>
   #   'public_profile'},
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secrets'], {:scope => 'public_profile'}
+  provider :facebook, ENV['app_id'], ENV['secrets'], {:scope => 'public_profile'}
 
 end

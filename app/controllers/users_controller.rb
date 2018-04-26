1bfaa53c5b6f369ac0f786495cf326496d151cbb
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-04-26 01:48:56 -0500

    users, models, routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+require 'dotenv'
 class UsersController < ApplicationController
   def index
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
+require 'dotenv'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'public_profile'}
+  provider :facebook, ENV['app_id'], ENV['secret'], {:scope => 'public_profile'}
 
 end

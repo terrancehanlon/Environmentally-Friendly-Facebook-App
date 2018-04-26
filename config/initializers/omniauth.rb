c7f9f00305f08b1f72184b1185b8c94315304194
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-04-26 01:52:01 -0500

    users, models, routes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
-require 'dotenv'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 584340308588152, 451de733ddb3998eee829a17ebaf6ca1, {:scope => 'public_profile'}
+  provider :facebook, 584340308588152, '451de733ddb3998eee829a17ebaf6ca1', {:scope => 'public_profile'}
 
 end

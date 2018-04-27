f19ad531397cb5e83bb4b3ea2cf7e516650e2751
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-04-26 20:52:54 -0500

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
   def login
     @user = User.koala(request.env['omniauth.auth']['credentials'])
 
-    if User.where(name == user['name']) do
-      
+    if User.where(user.name == user['name']) do
       User.create(name: @user['name'], email: 'myEmail@emailFroMIF.com')
     end
 
